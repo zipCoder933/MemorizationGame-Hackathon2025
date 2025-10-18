fe9e7145d61b3ec5312da14793537c447bb4d42d
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-10-18 09:14:14 -0600

    added

--- mock-dungeon-crawler/scenes/player/knight.gd
+++ mock-dungeon-crawler/scenes/player/knight.gd
@@ -14,6 +14,7 @@ var movement:Vector3 = Vector3.ZERO;
 var is_on_floor:bool = false
 const SPEED = 300;
 const TURN_SPEED = 5;
+var targetRotation:float;
 const ROTATION_LERP_SPEED = 0.2;
 
 
@@ -34,13 +35,27 @@ func _physics_process(delta: float) -> void:
 	#For top down third person movement
 	#linear_velocity.x = movement.x * SPEED * delta;
 	#linear_velocity.z = movement.z  * SPEED * delta;
-	#rotation.y = lerp_angle(rotation.y, atan2(linear_velocity.x, linear_velocity.z), ROTATION_LERP_SPEED)
+	#targetRotation = atan2(linear_velocity.x, linear_velocity.z)
+	#rotation.y = lerp_angle(rotation.y, targetRotation, ROTATION_LERP_SPEED)
 
 	#for immersive third person movement
-	rotation.y += movement.x * movement.z * delta * TURN_SPEED
-	var forward = transform.basis.z.normalized()  # Godot's "forward" is -Z
-	linear_velocity.x = forward.x * (movement.z * SPEED * delta)
-	linear_velocity.z = forward.z * (movement.z * SPEED * delta)
+	
+	#var forward = transform.basis.z.normalized()  # Godot's "forward" is -Z
+	var forward = Vector3.FORWARD.rotated(Vector3.RIGHT, phantom_camera_3d.get_third_person_rotation().x).rotated(Vector3.UP, phantom_camera_3d.get_third_person_rotation().y).normalized()
+	
+	if(movement.z == 0):
+		rotation.y = lerp_angle(rotation.y , targetRotation, delta * TURN_SPEED)
+		linear_velocity.x = forward.x * (abs(movement.x) * SPEED * delta)
+		linear_velocity.z = forward.z * (abs(movement.x) * SPEED * delta)
+	elif(movement.z < 0):
+		rotation.y = lerp_angle(rotation.y , targetRotation, delta * TURN_SPEED)
+		linear_velocity.x = forward.x * (abs(movement.z) * SPEED * delta)
+		linear_velocity.z = forward.z * (abs(movement.z) * SPEED * delta)
+	else:
+		targetRotation = rotation.y
+		rotation.y += movement.x * movement.z * delta * TURN_SPEED
+		linear_velocity.x = forward.x * (abs(movement.z) * SPEED * delta)
+		linear_velocity.z = forward.z * (abs(movement.z) * SPEED * delta)
 
 
 	if( linear_velocity.y > 0.5 ):
@@ -59,16 +74,21 @@ func _input(event: InputEvent) -> void:
 		
 	if Input.is_action_just_pressed("backward"):
 		movement.z = -1;
+		targetRotation = rotation.y+PI;
 	elif Input.is_action_just_released("backward"):
 		movement.z = 0;
 		
 	if Input.is_action_just_pressed("left"):
 		movement.x = 1;
+		if(movement.z == 0):
+			targetRotation = rotation.y+PI/2
 	elif Input.is_action_just_released("left"):
 		movement.x = 0;
 		
 	if Input.is_action_just_pressed("right"):
 		movement.x = -1;
+		if(movement.z == 0):
+			targetRotation = rotation.y-PI/2
 	elif Input.is_action_just_released("right"):
 		movement.x = 0;
 		
