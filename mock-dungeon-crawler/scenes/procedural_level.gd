a884616b576db15775362ec057e49332b5e54563
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-10-21 18:16:55 -0600

    Update procedural_level.gd

--- mock-dungeon-crawler/scenes/procedural_level.gd
+++ mock-dungeon-crawler/scenes/procedural_level.gd
@@ -59,50 +59,20 @@ func wall(x:float, z:float, res:Resource, z_axis:bool) -> Node3D:
 	add_entry(Vector3(x,0,z), instance)
 	return instance
 
-func removeIfSomethingHere(x:float, z:float) -> bool:
-	if get_entry(Vector3(x,0,z)).size() > 0:
-		remove_entries(Vector3(x,0,z))
-		return true
-	return false
-	
-func start(x:float, z:float):
-	if removeIfSomethingHere(x, z+0.5):
-		wall(x, z+0.5, DOOR,false)
-	else: 
-		wall(x, z+0.5, DOOR,false)
-	
-	if removeIfSomethingHere(x+1, z+0.5):
-		wall(x+1, z+0.5, DOOR,false)
-	else:
-		wall(x+1, z+0.5, DOOR,false)
-	
-	if removeIfSomethingHere(x+0.5, z):
-		wall(x+0.5, z, DOOR,true)
-	else:
-		wall(x+0.5, z, DOOR,true)
-	
-	if removeIfSomethingHere(x+0.5, z+1):
-		wall(x+0.5, z+1, DOOR,true)
-	else:
-		wall(x+0.5, z+1, DOOR,true)
-
-
 func box(x:int, z:int, keepX:bool, keepZ:bool):
 	_place_wall(x, z + 0.5, keepX,false)
 	_place_wall(x + 1, z + 0.5, keepX,false)
 	_place_wall(x + 0.5, z, keepZ,true)
 	_place_wall(x + 0.5, z + 1, keepZ,true)
 
-
-
 func _place_wall(x: float, z: float, keep: bool, z_axis:bool) -> void:
 	var coords = Vector3(x, 0, z)
 	var entry = get_entry(coords)
 	if entry.size() > 0:
-		if(keep):
-			pass
-		else:
+		if(!keep):
 			remove_entries(coords)
+			if(randf() > 0.9):
+				wall(x, z, DOOR,z_axis)
 	else:
 		wall(x, z, WALL,z_axis)
 
@@ -126,8 +96,7 @@ func _process(delta):
 	pass
 
 #THE MAP
-const map_start = Vector3(-50,0,-50)
-const map_end = Vector3(50,0,50)
+
 
 var counter:int = 0
 var searched: Dictionary = {}
@@ -154,21 +123,16 @@ func findDirectionThatPointsToTarget(end:Vector3) -> Direction:
 
 func pathDirection(direction:Direction, length:int, setBox:bool) -> bool:
 	var madeBox = false
-	print("Moving in direction: ",direction," length: ",length)
 	for j in range(length):
 		var tPlace = moveIn(place,direction)#Move temporarily
-		print("tplace: ",tPlace)
 		var tPlace2 = moveIn(tPlace,direction)#Move again
 		var goingX = (direction == Direction.XPOS or direction == Direction.XNEG)
 		
 		if searched.has(Vector3i(tPlace)) or searched.has(Vector3i(tPlace2)):
-			print("We've already been here")
 			break
 		elif goingX and (get_entry(Vector3(tPlace.x + 0.5,	 0, 	tPlace.z)).size() > 0 or get_entry(Vector3(tPlace.x + 0.5, 0, 	tPlace.z + 1)).size() > 0):
-			print("Walls cannot touch")
 			break
 		elif !goingX and (get_entry(Vector3(tPlace.x, 		 0,	tPlace.z + 0.5,)).size() > 0 or get_entry(Vector3(tPlace.x + 1,	 0,	tPlace.z + 0.5)).size() > 0):
-			print("Walls cannot touch")
 			break
 		else: #Place the box
 			place = tPlace
@@ -176,22 +140,35 @@ func pathDirection(direction:Direction, length:int, setBox:bool) -> bool:
 			madeBox=true
 			if(setBox):
 				if(direction == Direction.XPOS or direction == Direction.XNEG):
-					print("Placed box")
 					box(place.x,place.z,false,true)
 				else:
-					print("Placed box")
 					box(place.x,place.z,true,false)
 	return madeBox
 
-func _ready():
-	place = map_start
-	start(map_start.x,map_start.z)
-	for i in range(0,1000):
+func path(path_start:Vector3, path_end:Vector3, max_failures:int, randomness:float) -> int:
+	place = path_start
+	var stepsTaken = 0
+	var failures = 0
+	box(place.x, place.z,true,true)
+	
+	
+	for i in range(0, 100000):
 		var direction = Direction[Direction.keys()[randi_range(0,3)]]#random
-		if randf() > 0.2: #towards goal
-			direction = findDirectionThatPointsToTarget(map_end)
-		pathDirection(direction,randi_range(1,5),true)
-		
-		#if !pathDirection(Direction.XPOS,1,false) and !pathDirection(Direction.XNEG,1,false) and !pathDirection(Direction.ZPOS,1,false) and !pathDirection(Direction.XNEG,1,false):
-			#print("No more moves")
-			#return
+		if randf() > randomness: #towards goal
+			direction = findDirectionThatPointsToTarget(path_end)
+		if pathDirection(direction,randi_range(1,5),true):
+			stepsTaken +=1
+			failures = 0
+		else:
+			failures += 1
+			if(failures > max_failures):#If we already failed X times in a row, we can quit early
+				break
+	return stepsTaken
+
+func _ready():
+	print("MAIN: ", path(Vector3(-50,0,-50), Vector3(50,0,50),25,0))
+	
+	for i in range(0,200):
+		place = searched.keys()[randi_range(0,searched.keys().size()-1)]
+		var steps = path(place, Vector3(randi_range(-50,50),0,randi_range(-50,50)),25,0)
+		print("BRANCH: ", steps)
